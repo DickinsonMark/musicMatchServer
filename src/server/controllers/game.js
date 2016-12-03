247401f3016c3bb4f2cd18a0cdf93e90045835c2
DickinsonMark <monoposon@comcast.net>
2016-12-03 15:28:06 -0700

    removed console.logs

--- src/server/controllers/game.js
+++ src/server/controllers/game.js
@@ -1,15 +1,12 @@
 const knex = require('../db/connection.js');
 
 function addExperience(username, expGain, callBack) {
-  console.log(username, expGain);
   knex('users').where('username', username).then((user) => {
-    console.log(user[0].level);
     let totalExp = user[0].experience + Number(expGain);
     if (totalExp < (user[0].level * 10000)) {
       knex('users').where('username', username).update({
         experience: totalExp
       }).returning('*').then((user) => {
-        console.log(user);
         callBack(null, user);
       }).catch((err) => {
         callBack(err);
@@ -21,7 +18,6 @@ function addExperience(username, expGain, callBack) {
         experience: newExp,
         level: newLevel
       }).returning('*').then((user) => {
-        console.log(user);
         callBack(null, user);
       }).catch((err) => {
         callBack(err);
